c2e4782b91fb06885be2fee4f8bc625f8d9e0c1c
kfproductionkfp-crypto <kfproduction.kfp@gmail.com>
2026-03-02 15:40:24 -0300

    Delete newsletter JS directory

--- newsletter JS/newsletter JS newsletter.js
+++
@@ -1,9 +0,0 @@
-const newsletterForm = document.querySelector('.newsletter form');
-if (newsletterForm) {
-  newsletterForm.addEventListener('submit', function(e) {
-    e.preventDefault();
-    const email = this.querySelector('input').value;
-    alert(`Obrigado por se inscrever, ${email}!`);
-    this.reset();
-  });
-}
