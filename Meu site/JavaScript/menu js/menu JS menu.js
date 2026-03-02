fea57d56f0fcbcf0859acfd75ea9886640ce0a56
kfproductionkfp-crypto <kfproduction.kfp@gmail.com>
2026-03-02 14:51:49 -0300

    Delete Meu site/JavaScript directory

--- Meu site/JavaScript/menu js/menu JS menu.js
+++
@@ -1,8 +0,0 @@
-const menuToggle = document.querySelector('.menu-toggle');
-const nav = document.querySelector('header nav ul');
-
-if (menuToggle) {
-  menuToggle.addEventListener('click', () => {
-    nav.classList.toggle('active');
-  });
-}
