953b2247db53a2d57fb39c24da8412cd4f2c35bb
kfproductionkfp-crypto <kfproduction.kfp@gmail.com>
2026-03-02 17:36:45 -0300

    Delete menu.js

--- menu.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const toggle = document.getElementById("menu-toggle");
-  const list = document.getElementById("menu-list");
-
-  toggle.addEventListener("click", () => {
-    list.style.display = list.style.display === "block" ? "none" : "block";
-  });
-});
